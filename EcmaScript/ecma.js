db5ddcba4be2a31bac1615230587ac6b5bdc943d
Cristian Silva <cristian123105@gmail.com>
2020-05-16 12:31:37 -0300

    add ecma scrpt challenge

--- EcmaScript/ecma.js
+++ EcmaScript/ecma.js
@@ -1,5 +1,40 @@
 "use strict";
 //let e const
+var seraquepode = 'pode';
 console.log(seraquepode);
-var seraquepode = 'aaa';
+const dizer = () => console.log("kk eae men");
+dizer();
+function normalComThis() {
+    //console.log(this);
+}
+normalComThis();
+console.log(this);
+function attrDe(inicio = 3) {
+    console.log(inicio);
+}
+attrDe();
+attrDe(5);
+//spread e rest
+//...
+const number = [10, 12, 123, -5, 5234, 4123, 544, 54, 11, -34];
+const turmaA = ['João', 'carlos', 'josé'];
+const turmaB = ['jota', ...turmaA, 'ana',];
+console.log(Math.max(...number)); //os valores do array foram espalhados nos parametros da função
+console.log(turmaB);
+function retornarArray(a, ...numbers) {
+    console.log(a);
+    return numbers;
+}
+console.log(retornarArray('carro', 1, 2, 3, 4, 5, 67, 7, 45, 67, 5652, 43.3, 434, 3));
+//Rest & Spreat (Tupla)
+const tupla = [1, 'Cris', true];
+function Tupla(a, b, c) {
+    console.log(`A:${a} B:${b} C:${c}`);
+}
+Tupla(...tupla);
+function tupla2(...params) {
+    console.log(`2 ${params[0]} ${params[1]} ${params[2]}`);
+}
+tupla2(...tupla);
+const carro1 = ['Motor Rpidao', 2020];
 //# sourceMappingURL=ecma.js.map
--- EcmaScript/ecma.ts
+++ EcmaScript/ecma.ts
@@ -1,5 +1,57 @@
 //let e const
 
+var seraquepode = 'pode'
 console.log(seraquepode);
 
-var seraquepode = 'aaa'
+const dizer = () => console.log("kk eae men")
+
+dizer();
+
+function normalComThis(){
+    //console.log(this);
+}
+
+normalComThis();
+console.log(this)
+
+function attrDe(inicio: number = 3){
+    console.log(inicio);
+}
+
+attrDe()
+attrDe(5)
+//spread e rest
+//...
+
+const number = [10,12,123,-5,5234,4123,544,54,11,-34]
+
+const turmaA: string[] = ['João','carlos','josé'];
+const turmaB: string[] = ['jota',...turmaA,'ana',]
+
+console.log(Math.max(...number))//os valores do array foram espalhados nos parametros da função
+console.log(turmaB)
+
+function retornarArray(a:string,...numbers: number[]): number[]{
+    console.log(a)
+    return numbers
+}
+
+console.log(retornarArray('carro',1,2,3,4,5,67,7,45,67,5652,43.3,434,3));
+
+//Rest & Spreat (Tupla)
+
+const tupla:[number, string, boolean] = [1,'Cris',true];
+
+function Tupla(a:number,b: string, c: boolean):void{
+    console.log(`A:${a} B:${b} C:${c}`)
+}
+
+Tupla(...tupla)
+
+function tupla2(...params:[number,string,boolean]): void{
+    console.log(`2 ${params[0]} ${params[1]} ${params[2]}`)
+}
+
+tupla2(...tupla)
+
+const carro1:[string,number] = ['Motor Rpidao',2020]
