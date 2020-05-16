7f6a2ad7ef788d34b077ba77d712883c769d03f6
Cristian Silva <cristian123105@gmail.com>
2020-05-16 13:39:22 -0300

    init class

---
+++ classes/class.js
@@ -0,0 +1,10 @@
+"use strict";
+class Data {
+    constructor(dia, mes, ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
+const aniversario = new Data(11, 1, 1999);
+//# sourceMappingURL=class.js.map
---
+++ classes/class.ts
@@ -0,0 +1,14 @@
+class Data {
+    dia: number
+    mes: number
+    ano: number
+
+    constructor(dia: number, mes: number, ano: number){
+        this.dia = dia
+        this.mes = mes
+        this.ano = ano
+    }
+
+}
+
+const aniversario = new Data(11,1,1999)
