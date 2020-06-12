449f537dbbeba886525e5037bfcb00ee67a1e4bf
Cristian Silva <cristian123105@gmail.com>
2020-06-12 09:24:13 -0300

    webpack module

---
+++ integracao-webpack/src/main.ts
@@ -0,0 +1,8 @@
+import Livro from './model/livro';
+import $ from 'jquery';
+
+const livro = new Livro('O Bixão',100,1);
+
+$('body').append(`<h1>${livro.nome}</h1>`)
+$('body').append(`<h2>Preço: R$${-(livro.precoComDesconto())}</h2>`)
+$('body').append(`<h2>aaaaaaaaaaaaaaa</h2>`)
